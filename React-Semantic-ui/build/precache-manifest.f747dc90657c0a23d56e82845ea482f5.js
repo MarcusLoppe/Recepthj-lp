5f535426b2a408210ed6d1908ab980a6dcf3a7d3
MarcusLoppe <marcusloren1993@gmail.com>
2020-05-15 17:54:13 +0200

    a

---
+++ React-Semantic-ui/build/precache-manifest.f747dc90657c0a23d56e82845ea482f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c914d6ed11951c89bd048713fad068b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65725629c500019a3ae6",
+    "url": "/static/js/2.470a2e6a.chunk.js"
+  },
+  {
+    "revision": "189990ea2162248a90c51db484804b53",
+    "url": "/static/js/2.470a2e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8e78c5bce6d8f81e2d",
+    "url": "/static/js/main.149ff1b8.chunk.js"
+  },
+  {
+    "revision": "db24628202892ae94ffb",
+    "url": "/static/js/runtime-main.b75af658.js"
+  }
+]);
